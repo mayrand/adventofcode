50c4a2c0ce7936b8c3ec75ed105a9a05f56b3e0e
Andrzej Ołdak <mayrand@tlen.pl>
2019-12-07 12:40:27 +0100

    Day 1 complete

---
+++ 2019/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.IO.File.ReadAllLines("input").Select(v => int.Parse(v)).Sum(x => x / 3 - 2));
+        }
+    }
+}
