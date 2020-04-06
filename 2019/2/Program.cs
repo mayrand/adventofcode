15e8486e4526bf5bc24e5e0e7bba6a5423b57413
Andrzej Ołdak <mayrand@tlen.pl>
2020-04-06 13:11:59 +0200

    Day 2 first part

---
+++ 2019/2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("input").Split(',').Select(int.Parse).ToArray();
+            IntCode(ref data);
+            Console.WriteLine(string.Join(", ", data));
+        }
+
+        static void IntCode(ref int[] data)
+        {
+            int x, y, z;
+            for (var i = 0; data[i] < 99; i += 4)
+            {
+                x = data[i + 1];
+                y = data[i + 2];
+                z = data[i + 3];
+                switch (data[i])
+                {
+                    case 1:
+                        data[z] = data[x] + data[y];
+                        break;
+                    case 2:
+                        data[z] = data[x] * data[y];
+                        break;
+                    default:
+                        throw new Exception("Unknown opcode");
+                }
+            }
+            
+        }
+    }
+}
