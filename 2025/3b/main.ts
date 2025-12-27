af00cc5721ddae2236f2995a2cb66b2aad28fa9c
andrzejoldak <andrzej.oldak@hotmail.com>
2025-12-27 12:45:00 +0100

    3b

--- 2025/3b/main.ts
+++ 2025/3b/main.ts
@@ -12,54 +12,21 @@ if (import.meta.main) {
   let result = 0n;
   for (const line of lines) {
     result += getMaxJoltage(line);
-    console.log(result);
   }
   console.log(result);
 }
 
 export function getMaxJoltage(bank: string): bigint {
-  let striped = getHighestFromFour(bank);
-  if (striped.length === 12) {
-    return BigInt(striped);
-  }
-  let first = [10, -1];
-  let second = [10, -1];
-  let third = [10, -1];
-  for (let i = 0; i < striped.length; i++) {
-    const num = Number.parseInt(striped[i]);
-    if (num < first[0]) {
-      first[0] = num;
-      first[1] = i;
-    } else if (num < second[0]) {
-      second[0] = num;
-      second[1] = i;
-    } else if (num < third[0]) {
-      third[0] = num;
-      third[1] = i;
+  const positions: number[] = Array(12).fill(0);
+  let start = 0;
+  for (let pos = 0; pos < 12; pos++) {
+    const maxLen = bank.length - 12 + pos
+    for (let i = start; i <= maxLen; i++) {
+      const num = Number.parseInt(bank[i]);
+      if (num <= positions[pos]) continue;
+      positions[pos] = num;
+      start = i + 1;
     }
   }
-    
-  return BigInt(
-    removeCharAtPosition(
-      removeCharAtPosition(removeCharAtPosition(striped, first[1]), second[1] - 1),
-      third[1] - 2,
-    ),
-  );
-}
-
-export function removeCharAtPosition(str: string, position: number, minLen: number = 12): string {
-  if(str.length <= minLen) return str;
-  return str.slice(0, position) + str.slice(position + 1);
-}
-
-export function getHighestFromFour(bank: string): string {
-  let max = [0, -1];
-  for (let i = 0; i < 4; i++) {
-    const num = Number.parseInt(bank[i]);
-    if (num > max[0]) {
-      max[0] = num;
-      max[1] = i;
-    }
-  }
-  return bank.slice(max[1]);
+  return BigInt(positions.join(""));
 }
